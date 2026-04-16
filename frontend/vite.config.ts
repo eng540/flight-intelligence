73b9d4e2fd19ef3561803f5cd770600a4674cbc0
eng540 <132091714+eng540@users.noreply.github.com>
2026-04-16 19:23:43 +0300

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,13 @@
-import { fileURLToPath, URL } from "node:url"
-import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import path from "path"
 
-// https://vite.dev/config/
 export default defineConfig({
   base: '/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
